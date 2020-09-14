0d95c8be2214b88d65c6bd31337ebacde9c652c3
lethanh11 <lethanh110700@gmail.com>
2020-09-14 15:48:00 +0700

    update header

---
+++ header.php
@@ -0,0 +1,148 @@
+<div class="grid">
+  <div class="header-top">
+    <nav class="header__navbar">
+      <ul class="header__navbar-list">
+        <!-- <li class="header__navbar-item ">
+          <a href="" class="header__navbar-item-link"> ĐĂNG NHẬP / ĐĂNG KÝ </a>
+        </li> -->
+        <li class="header__navbar-item header__navbar-user">
+          <img src="https://scontent.fdad3-1.fna.fbcdn.net/v/t1.0-1/cp0/p80x80/82081289_754571055024199_2525229642340630528_n.jpg?_nc_cat=103&_nc_sid=7206a8&_nc_ohc=u0-ksiVxtFwAX9HDjYL&_nc_ht=scontent.fdad3-1.fna&oh=15ec43dda9ace9935c8907fd93977022&oe=5F83B968" alt="" class="header__navbar-user-img">
+          <a href="" class="header__navbar-user-name">Lê Thạnh</a>
+
+          <ul class="header__navbar-user-menu">
+            <li class="header__navbar-user-item">
+              <a href="">Tài Khoản của tôi</a>
+            </li>
+            <li class="header__navbar-user-item">
+              <a href="">Địa chỉ của tôi</a>
+            </li>
+            <li class="header__navbar-user-item">
+              <a href="">Đơn Mua</a>
+            </li>
+            <li class="header__navbar-user-item">
+              <a href="">Đăng Xuất</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <ul>
+        <li class="header__navbar-item">
+          <a href=""> <img width="180" height="60" src="logo-mona.png" alt="" /></a>
+        </li>
+      </ul>
+      <ul class="header__navbar-list ">
+        <li class="header__navbar-item header__navbar-item--search header__navbar-item--separate ">
+          <a href="" class="header__navbar-item-link"> <i class="fas fa-search"></i></a>
+          <!-- /* Header Search code */ -->
+          <div class="header__search">
+            <input type="text" class="header__text" placeholder="Tìm Kiếm...">
+            <div class="header__logo">
+              <button type="submit" class="header__item"><i class="fas fa-search"></i></button>
+            </div>
+          </div>
+        </li>
+        <li class="header__navbar-item ">
+          <div class="header__cart">
+            <a href="" class="header__navbar-item-link">GIỎ HÀNG / 0 ₫ &nbsp; <i class="fas fa-shopping-cart"></i></a>
+            <span class="header__cart-notice">3</span>
+            <!-- No cart: header_cart-list--no-cart -->
+            <div class="header_cart--list header__cart-list--no-cart">
+              <img src="empty-cart.png" class="header__cart-no-cart-img" alt="">
+              <!-- <span class="header__cart-list-no-cart-msg">Chưa có sản phẩm</span> -->
+              <ul class="header__cart-list-item">
+                <li class="header__cart-item">
+                  <img src="http://mauweb.monamedia.net/converse/wp-content/uploads/2019/05/women-classic-3-300x225.jpg" alt="" class="header__cart-img">
+                  <div class="header__cart-item-infor">
+                    <div class="header__cart-item-head">
+                      <h4 class="header__cart-item-name">Chuck Taylor Classic</h4>
+                      <div class="header__cart-item-price-wrap">
+                        <span class="header__cart-item-price">2.000.000đ</span>
+                        <span class="header__cart-item-multiply">x</span>
+                        <span class="header__cart-item-quantity">2</span>
+                      </div>
+                    </div>
+                    <div class="header__cart-item-body">
+                      <span class="header__cart-item-species">Phân Loại : Classic</span>
+                      <span class="header__cart-item-delete">Xóa</span>
+
+                    </div>
+                  </div>
+                </li>
+                <li class="header__cart-item">
+                  <img src="http://mauweb.monamedia.net/converse/wp-content/uploads/2019/05/women-classic-3-300x225.jpg" alt="" class="header__cart-img">
+                  <div class="header__cart-item-infor">
+                    <div class="header__cart-item-head">
+                      <h4 class="header__cart-item-name">Chuck Taylor Classic</h4>
+                      <div class="header__cart-item-price-wrap">
+                        <span class="header__cart-item-price">2.000.000đ</span>
+                        <span class="header__cart-item-multiply">x</span>
+                        <span class="header__cart-item-quantity">2</span>
+                      </div>
+                    </div>
+                    <div class="header__cart-item-body">
+                      <span class="header__cart-item-species">Phân Loại : Classic</span>
+                      <span class="header__cart-item-delete">Xóa</span>
+
+                    </div>
+                  </div>
+                </li>
+                <li class="header__cart-item">
+                  <img src="http://mauweb.monamedia.net/converse/wp-content/uploads/2019/05/women-classic-3-300x225.jpg" alt="" class="header__cart-img">
+                  <div class="header__cart-item-infor">
+                    <div class="header__cart-item-head">
+                      <h4 class="header__cart-item-name">Chuck Taylor Classic</h4>
+                      <div class="header__cart-item-price-wrap">
+                        <span class="header__cart-item-price">2.000.000đ</span>
+                        <span class="header__cart-item-multiply">x</span>
+                        <span class="header__cart-item-quantity">2</span>
+                      </div>
+                    </div>
+                    <div class="header__cart-item-body">
+                      <span class="header__cart-item-species">Phân Loại : Classic</span>
+                      <span class="header__cart-item-delete">Xóa</span>
+
+                    </div>
+                  </div>
+                </li>
+              </ul>
+              <a class="header__cart-view-cart">Xem Giỏ Hàng</a>
+              <a class="header__cart-checkout  ">Thanh Toán</a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>
+<div class="header-bottom">
+  <nav class="header__navbar-bottom">
+    <ul class="menu-list">
+      <li class="menu-list__item active"><a class="border" href="#">trang chủ</a></li>
+      <li class="menu-list__item "><a class="border" href="#">giới thiệu</a></li>
+      <li class="menu-list__item menu-list__item-women ">
+        <a class="border" href="#"> nữ <i class="far fa-angle-down"></i></a>
+        <ul class="menu-list menu-list--women">
+          <li class="menu-list__item--women"><a href="">Classic</a> </li>
+          <li class="menu-list__item--women"><a href="">Sunbaked</a></li>
+          <li class="menu-list__item--women"><a href="">Chuck 07s</a> </li>
+          <li class="menu-list__item--women"><a href="">One Star</a> </li>
+          <li class="menu-list__item--women"><a href="">PSY-Kicks</a> </li>
+        </ul>
+      </li>
+      <li class="menu-list__item menu-list__item-man">
+        <a class="border" href="#">nam <i class="far fa-angle-down"></i></a>
+        <ul class="menu-list menu-list--man">
+          <li class="menu-list__item--man"><a href="">Classic</a></li>
+          <li class="menu-list__item--man"><a href="">Sunbaked</a></li>
+          <li class="menu-list__item--man"><a href="">Chuck 07s</a></li>
+          <li class="menu-list__item--man"><a href="">One Star</a></li>
+          <li class="menu-list__item--man"><a href="">PSY-Kicks</a></li>
+        </ul>
+      </li>
+      <li class="menu-list__item"><a class="border" href="#">trẻ em</a></li>
+      <li class="menu-list__item"><a class="border" href="#">phụ kiện khác</a></li>
+      <li class="menu-list__item"><a class="border" href="#">tin tức</a></li>
+      <li class="menu-list__item"><a class="border" href="#">liên hệ</a></li>
+    </ul>
+  </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="base.css" />
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="app">
+    <header class="header">
+      <?php include "header.php" ?>
+    </header>
+
+    <div class="container"></div>
+
+    <div class="footer"></div>
+  </div>
+
+  <!-- Modal -->
+  <!-- <div class="modal">
+    <div class="modal__overlay">
+
+    </div>
+    <div class="modal__body">
+      Đăng Ký
+        <div class="auth-form">
+         <div class="auth-form__container">
+         <div class="auth-form__header">
+            <h3 class="auth-form__heading">Đăng Ký</h3>
+            <span class="auth-form__switch-btn">Đăng Nhập</span>
+          </div>
+          <div class="auth-form__form">
+            <div class="auth-form__group">
+              <label for="" class="auth-form__text">Địa chỉ email *</label>
+              <input type="text" class="auth-form__input" placeholder="Nhập Email">
+            </div>
+            <div class="auth-form__group">
+            <label for="" class="auth-form__text">Mật Khẩu *</label>
+              <input type="password" class="auth-form__input" placeholder="Nhập Mật khẩu">
+            </div>
+            <div class="auth-form__group">
+            <label for="" class="auth-form__text">Nhập lại mật khẩu *</label>
+              <input type="password" class="auth-form__input" placeholder="Nhập lại mật khẩu">
+            </div>
+          </div>
+          <div class="auth-form__aside">
+            <p class="auth-form__policy-text">
+              Bằng việc đăng ký, bạn đã đồng ý với Mona về
+              <a href="" class="auth-form__text-link">Điều khoản dịch vụ</a> &
+              <a href="" class="auth-form__text-link"> Chính sách bảo mật </a>
+            </p>
+          </div>
+          <div class="auth-form__controls">
+            <buttton text="submit" class="btn btn-normal auth-form__controls-back">TRỞ LẠI</buttton>
+            <buttton text="submit" class="btn btn--primary ">ĐĂNG KÝ</buttton>
+          </div>
+         </div>
+          <div class="auth-form__socials">
+            <a href="" class="auth-form__socials--facebook btn btn--size-s btn--with-icon">
+            <i class="auth-form__socials-icon fab fa-facebook-square"></i>
+           <span class="auth-form__socials-title">Kết nối với Facebook</span> 
+            </a>
+            <a href=""  class="auth-form__socials--google btn  btn--size-s btn--with-icon">
+            <i style="color: #666;" class="auth-form__socials-icon fab fa-google"></i>
+            <span class="auth-form__socials-title"> Kết nối với Google</span>
+            </a>
+          </div>
+        </div>
+       Đăng Nhập
+       <div class="auth-form">
+         <div class="auth-form__container">
+         <div class="auth-form__header">
+            <h3 class="auth-form__heading">Đăng Nhập</h3>
+            <span class="auth-form__switch-btn">Đăng Ký</span>
+          </div>
+          <div class="auth-form__form">
+            <div class="auth-form__group">
+              <label for="" class="auth-form__text">Địa chỉ email *</label>
+              <input type="text" class="auth-form__input" placeholder="Nhập Email">
+            </div>
+            <div class="auth-form__group">
+            <label for="" class="auth-form__text">Mật Khẩu *</label>
+              <input type="password" class="auth-form__input" placeholder="Nhập Mật khẩu">
+            </div>
+           
+          </div>
+          <div class="auth-form__aside">
+           <div class="auth-form__help">
+             <a class="auth-form__help-link auth-form__help-forgot">Quên mật khẩu</a>
+             <span class="auth-form__help-separate"></span>
+             <a class="auth-form__help-link "> Cần trợ giúp?</a>
+           </div>
+          </div>
+          <div class="auth-form__controls">
+            <buttton text="submit" class="btn btn-normal auth-form__controls-back">TRỞ LẠI</buttton>
+            <buttton text="submit" class="btn btn--primary ">ĐĂNG NHẬP</buttton>
+          </div>
+         </div>
+          <div class="auth-form__socials">
+            <a href="" class="auth-form__socials--facebook btn btn--size-s btn--with-icon">
+            <i class="auth-form__socials-icon fab fa-facebook-square"></i>
+           <span class="auth-form__socials-title">Kết nối với Facebook</span> 
+            </a>
+            <a href=""  class="auth-form__socials--google btn  btn--size-s btn--with-icon">
+            <i style="color: #666;" class="auth-form__socials-icon fab fa-google"></i>
+            <span class="auth-form__socials-title"> Kết nối với Google</span>
+            </a>
+          </div>
+        </div>
+    </div>
+  </div> -->
+</body>
+
+</html>
